6b3e36fa4ad2007e1f266d26a5841b1305e9e8e1
Isaac Yriarte <isaacyagi@gmail.com>
2020-08-16 18:58:53 -0500

    Feat: Agrega servicio de obtener heroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -31,4 +31,26 @@ export class HeroesService {
 
     return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp);
   }
+
+  getHeroes(){
+    return this.http.get(`${this.url}/heroes.json`).pipe(
+      map(resp => this.crearArregloHeroes(resp))
+      // map(this.crearArregloHeroes)
+    );
+  }
+
+  private crearArregloHeroes(heroesObj: Object){
+    const heroes: HeroeModel[] = [];
+
+    if (heroesObj === null) return [];
+    
+    Object.keys( heroesObj).forEach(key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+
+      heroes.push(heroe);
+    })
+    
+    return heroes;
+  }
 }
