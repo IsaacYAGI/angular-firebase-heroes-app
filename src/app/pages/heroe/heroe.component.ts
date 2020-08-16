ecbbf8ebc9ee5ee2385d66ba26e590ca15c9d6a0
Isaac Yriarte <isaacyagi@gmail.com>
2020-08-16 17:44:26 -0500

    Feat: Implementa servicios en el componente de heroe

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/models/heroe.model';
 import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -10,7 +11,9 @@ import { NgForm } from '@angular/forms';
 export class HeroeComponent implements OnInit {
 
   heroe = new HeroeModel();
-  constructor() { }
+  constructor(
+    private heroesService: HeroesService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -21,9 +24,10 @@ export class HeroeComponent implements OnInit {
       console.log("Formulario invalido");
       return;
     }
-    
-    console.log(form);
-    console.log(this.heroe);
+    this.heroesService.crearHeroe(this.heroe)
+      .subscribe(resp =>{
+        console.log(resp)
+      });
   }
 
 }
