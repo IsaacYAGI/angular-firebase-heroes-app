cae250cbe98696f09fe9d22b896da9389ea4c46f
Isaac Yriarte <isaacyagi@gmail.com>
2020-08-16 20:15:54 -0500

    Feat: Integra servicio de eliminacion en la pantalla de heroes

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { HeroeModel } from 'src/models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -20,6 +21,25 @@ export class HeroesComponent implements OnInit {
     this.heroesService.getHeroes().subscribe(resp => {
       this.heroes = resp;
     });
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number){
+
+    Swal.fire({
+      title:"Esta seguro?",
+      text: `Está seguro que desea borrar a ${heroe.nombre}?`,
+      icon: 'question',
+      showConfirmButton:true,
+      showCancelButton: true
+    }).then(resp =>{
+      if (resp.value){
+        this.heroesService.borrarHeroe(heroe.id).subscribe((resp) =>{
+          this.heroes.splice(i,1);
+        });
+
+      }
+    });
+
   }
 
 }
